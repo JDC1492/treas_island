315277683b61bc4e765641a381b20f569003b1c4
Jonathan Cohen <65975460+JDC1492@users.noreply.github.com>
2021-08-29 19:53:00 +0000

    completed.

--- main.py
+++ main.py
@@ -21,11 +21,24 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 *******************************************************************************
 ''')
 print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.") 
-direction = input("You arrive at a fork in the path. Do you go left or right?").lower() 
+print("Your mission is to find the treasure.")
+direction = input(
+    "You arrive at a fork in the path. Do you go left or right?\n").lower()
 if (direction == "left"):
-  action = input("You come to a lake. There is an island in the middle of the lake. Type 'wait' for a boat. Type 'swim' to swim across.").lower()
-else:
-  print("")
+  action = input("You come to a lake. There is an island in the middle of the lake. Type 'wait' for a boat. Type 'swim' to swim across.\n").lower()
+  if action == "wait":
+    door = input("You arrive to the distant island unharmed. There's a house with 3 doors of the colors red yellow and blue. Which do you choose?\n").lower()
+    if door == "red":
+      print("You've been burned by fire. Game Over.")
+    elif door == "blue":
+      print("You've been eaten by beasts. Game Over.")
+    elif door == "yellow":
+      print('You Win!')
+    else:
+      print("Game Over.")
+  elif action != "wait" :
+    print("You've been attacked by an incredibly large trout. Game Over.")
+elif direction != "left":
+    print("You've fallen into a hole. Game Over.")
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
