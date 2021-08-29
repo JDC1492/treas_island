f0741244993adb515510f724367f3945493ed129
Jonathan Cohen <65975460+JDC1492@users.noreply.github.com>
2021-08-29 19:17:12 +0000

    successfully answered the first question and was able to move forward in the game.

--- main.py
+++ main.py
@@ -22,5 +22,10 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
+direction = input("You arrive at a fork in the path. Do you go left or right?").lower() 
+if (direction == "left"):
+  action = input("You come to a lake. There is an island in the middle of the lake. Type 'wait' for a boat. Type 'swim' to swim across.").lower()
+else:
+  print("")
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
